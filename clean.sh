86882ef5f65a4fe7a206c3e3d8788edeeb46e05f
Will Breiler <will.breiler@gmail.com>
2021-11-16 12:50:50 -0600

    Finished clean up script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-echo "Removing volumes, please wait."
-sleep 5
-docker volume rm minecraft-data hass-data
-echo "Removing services, please wait."
-docker service rm minecraft 
+echo "Would you like to remove the volumes?"
+select yn in "Yes" "No"; do
+		case $yn in
+				Yes )
+						docker volume rm $(docker volume ls -q);
+						echo "Volumes removed";
+						break;;
+				No ) exit;;
+		esac
+done
+
+echo "Would you like to remove the containers?"
+select yn in "Yes" "No"; do
+		case $yn in
+				Yes )
+						docker rm $(docker ps -a -q);
+						echo "Containers removed";
+						break;;
+				No ) exit;;
+		esac
+done
